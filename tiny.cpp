262b98ab4cb31b90bf46ac781f6a499a6fea6d38
bovenzi <boz119@foxmail.com>
2020-05-10 18:03:11 -0400

    doit,client error handle finished

--- tiny.cpp
+++ tiny.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 
 // 函数声明
+
 void doit(int fd);
 
 void read_requesthdrs(rio_t *rp);
@@ -50,3 +51,137 @@ int main(int argc, char **argv){
 	}
 	exit(0);
 }
+
+
+
+void doit(int fd)
+{
+	int is_static;
+	struct stat sbuf;
+	char buf[MAXLINE];
+	char method[MAXLINE];
+	char uri[MAXLINE];
+	char version[MAXLINE];
+	char filename[MAXLINE];
+	char cgiagrs[MAXLINE];
+	rio_t rio;
+	
+	// 读取请求和header
+
+	Rio_readinitb(&rio, fd);	
+	Rio_readlineb(&rio, buf MAXLINE);
+	sscanf(buf, "%s %s %s", method, url , version);
+
+	if(strcasecmp(method, "GET")){ // strcasecmp is a CASE-ignored compared function, if same, return 0
+		clienterror(fd, method, "501", "Not Implemented", "Tiny does not implement this method");
+		return ;	
+	}
+	
+	read_requesthdrs(&rio);
+	
+	// 从GET请求中获取 URI 
+	is_static = parse_uri(uri, filename, cgiargs); //parse_uri function will implenment later
+
+	if( stat(filename,&sbuf) < 0){   // stat() is function in sys/stat.h, which is used to obtain the status of file(filename) and purse into buf
+		clienterror(fd, filename, "404", "Not found", "Tiny cannot find this file");
+		return;	
+	}
+	//处理静态请求 static 
+	if(is_static){
+		if(!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode) ){
+			clienterror(fd,filename,"403", "Forbidden", "Tiny cannot read the file");
+			return;		
+		}
+		server_static(fd, filename, sbuf.st_size);
+	}
+	//处理动态请求 dynamic
+	else{
+		if(!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)){
+			clienterror(fd,filename,"403", "Forbidden" , "Tiny cannot run the CGI app");
+			return;
+		}
+		serve_dynamic(fd, filename, cgiargs);	
+	}
+}
+
+
+
+/*
+
+	clienterror function throw a error message to the client 
+*/
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg){
+	char buf[MAXLINE];
+	char body[MAXBUF];
+	
+	// http body
+	sprintf(body, "<html><title>Tiny Error</title>");
+	sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
+	sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+	sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+	sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
+	
+	// http response
+	sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+	Rio_writen(fd, buf, strlen(buf));
+	sprintf(buf, "Content-type: text/html\r\n");
+	Rio_writen(fd, buf, strlen(buf));
+	sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlne(body));
+	Rio_writen(fd, buf, strlen(buf));
+	Rio_writen(fd, body, strlen(body));	
+}
+
+
+/*
+	Reads and ignores request hearders
+*/
+void read_requesthdrs(rio_t *rp){
+	char buf[MAXLINE];
+	Rio_readlineb(rp, buf, MAXLINE);
+	while(strcmp(buf, "\r\n")){
+		Rio_readlineb(rp,buf,MAXLINE);
+		printf("%s",buf);/
+	}
+	return;
+}
+
+
+/*
+	
+*/
+int parse_uri(char *uri, char *filename, char *cgiargs){
+	char *ptr;
+	
+	//C 库函数 char *strstr(const char *haystack, const char *needle) 在字符串 haystack 中查找第一次出现字符串 needle 的位置，不包含终止符 '\0'
+	if(!strstr(uri, "cgi-bin")){ // static 
+		strcpy(cgiargs, "");
+		strcpy(filename,".");
+		strcat(filename, uri);
+		if(uri[strlen(uri)-1] == '/')
+			strcat(filename, "home.html");
+		return 1;
+	}
+	else{ // dynamic 
+		ptr = index(uri, '?');
+		if(ptr) {
+			strcpy(cgiargs,ptr+1);
+			*ptr = '\0';
+		}
+		else{
+			strcpy(cgiargs, "");
+		}
+		strcpy(filename , ".");
+		strcat(filename, uri);
+		return 0;
+	}
+} 
+
+
+
+
+
+
+
+
+
+
